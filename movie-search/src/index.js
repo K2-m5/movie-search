51f2b27cb215d73d14b101b9194dab2f4f96de4f
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-05-10 22:05:38 +0300

    refactor: remove files Controller, View, logic initializeMarkup add to index

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -1,49 +1,17 @@
 import './style.css';
 
-import initializeMarkup from './View';
+import StructureHtml from './components/StructureHtml/StructureHtml';
+import Fetch from './components/Fetch/Fetch';
 
+async function initializeMarkup() {
+  const fetchObj = new Fetch();
+  const movie = 'Transformers';
+  const data = await fetchObj.searchMovie(movie);
+  console.log(data);
+
+  const structureHTML = new StructureHtml(document.body, data);
+
+  structureHTML.createMarkup();
+}
 
 initializeMarkup();
-
-// async function newFetch() {
-//   await initializeMarkup();
-
-//   const searchBtn = document.querySelector('.search-btn');
-
-//   function searchMovie() {
-//     const form = document.forms.search;
-//     const movie = form[0].value.toString();
-//     searchBtn.event = initializeMarkup(movie);
-//   }
-
-//   searchBtn.addEventListener('click', searchMovie);
-// }
-
-
-// const form = document.forms.search;
-
-
-
-// const searchBtn = document.querySelector('.search-btn');
-// const form = document.forms.search;
-// const valueForm = form[0].value.toString();
-// const cards = [];
-// const wrapper = document.querySelector('.swiper-wrapper');
-
-// async function setFetch() {
-//   const fetchObj = new Fetch();
-//   const data = await fetchObj.searchMovie(valueForm);
-
-//   for (let i = 0; i < data.length; i += 1) {
-//     cards.push(new Card(data[i]));
-//   }
-
-//   function render() {
-//     for (let i = 0; i < cards.length; i += 1) {
-//       wrapper.appendChild(cards[i].render());
-//     }
-//   }
-//   render(data);
-// }
-
-// searchBtn.addEventListener('click', setFetch(valueForm));
