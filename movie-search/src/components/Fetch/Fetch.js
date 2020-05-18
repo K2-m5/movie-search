09b429c09868017bd6c70a29d242a7f0737791f4
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-05-18 16:06:02 +0300

    feat: update function fetch

--- movie-search/src/components/Fetch/Fetch.js
+++ movie-search/src/components/Fetch/Fetch.js
@@ -11,31 +11,53 @@ export default class Fetch {
 
   async searchMovie(searchString, page = 1) {
     const url = this.getUrl(`s=${searchString}&page=${page}`);
-    const response = await fetch(url);
-    const data = await response.json();
-
-    if (data.Response !== 'True') {
-      return null;
-    }
-
-    const moviesTasks = data.Search.map((item) => {
-      return this.getMovieInfo(item.imdbID);
+    const data = await fetch(url).then(response => response.json()).then(result => {
+      return this.getMovieInfo(result);
     });
-
-    const movies = await Promise.all(moviesTasks);
-
     return {
-      moviesList: movies,
+      moviesList: data,
       totalResults: data.totalResults,
       searchString: searchString
     };
   }
 
-  async getMovieInfo(movieId) {
-    const url = this.getUrl(`i=${movieId}`);
-    const resp = await fetch(url);
-    const movieInfo = await resp.json();
-
-    return movieInfo;
+  async getMovieInfo(dataList) {
+    const moviesTasks = dataList.Search;
+    const movieInfo = moviesTasks.map(async item => {
+      await fetch(this.getUrl(`i=${item.imdbID}`)).then(response => response.json()).then(movieItem => {
+        item = movieItem;
+      });
+      return item;
+    });
+    return Promise.all(movieInfo);
   }
 }
+
+// async searchMovie(searchString, page = 1) {
+//   const url = this.getUrl(`s=${searchString}&page=${page}`);
+//   const response = await fetch(url);
+//   const data = await response.json();
+
+//   if (data.Response !== 'True') {
+//     return null;
+//   }
+
+//   const moviesTasks = data.Search.map((item) => {
+//     return this.getMovieInfo(item.imdbID);
+//   });
+
+//   const movies = await Promise.all(moviesTasks);
+
+//   return {
+//     moviesList: movies,
+//     totalResults: data.totalResults,
+//     searchString: searchString
+//   };
+// }
+
+// async getMovieInfo(movieId) {
+//   const url = this.getUrl(`i=${movieId}`);
+//   const resp = await fetch(url);
+//   const movieInfo = await resp.json();
+
+//   return movieInfo;
